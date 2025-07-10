cac6e201bd911d4980d39159e2aed6755eccf225
Tomy142 <tomaslema.01@gmail.com>
2025-07-10 19:27:12 -0300

    add car, add act 3

---
+++ sql/actividad-3/script.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE IF NOT EXISTS group_user
+    DEFAULT CHARACTER SET utf8
+    COLLATE utf8_bin;
+
+USE group_user;
+
+/*---Crear Tabla User---*/
+
+CREATE TABLE user(
+    id_user INT 
+)
