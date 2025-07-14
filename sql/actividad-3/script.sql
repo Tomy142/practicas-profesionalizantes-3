3201b2f9411f8b1c1f7f9a654f6c106b1ba774c6
Tomy142 <tomaslema.01@gmail.com>
2025-07-14 13:01:19 -0300

    brain storming

--- sql/actividad-3/script.sql
+++ sql/actividad-3/script.sql
@@ -7,5 +7,31 @@ USE group_user;
 /*---Crear Tabla User---*/
 
 CREATE TABLE user(
-    id_user INT 
+    id_user INT UNSIGNED AUTO_INCREMENT PRIMARY KEY UNIQUE NOT NULL,
+    name VARCHAR(45) UNIQUE NOT NULL,
+    password VARCHAR(128) NOT NULL
+)
+
+/*----Crear Tabla Group---*/
+
+CREATE TABLE group(
+    id_group INT UNSIGNED AUTO_INCREMENT PRIMARY KEY UNIQUE NOT NULL,
+    name VARCHAR(45) UNIQUE NOT NULL,
+    description VARCHAR(128) 
+)
+
+/*---Crear Tabla Action---*/
+
+CREATE TABLE action(
+    id_group INT UNSIGNED AUTO_INCREMENT PRIMARY KEY UNIQUE NOT NULL,
+    name VARCHAR(45) UNIQUE NOT NULL,
+    description VARCHAR(128),
+)
+
+/*---Crear Tabla Web-Api ---*/
+
+CREATE TABLE action(
+    id_group INT UNSIGNED AUTO_INCREMENT PRIMARY KEY UNIQUE NOT NULL,
+    url VARCHAR(256) UNIQUE NOT NULL,
+    http_method VARCHAR(45)
 )
